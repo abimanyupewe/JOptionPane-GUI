19d0bddbe5fb0e926d614766ed4d0452089daca7
D-8 <D-8@D-8>
2024-10-28 11:37:10 +0700

    pertemuan 5

---
+++ pertemuan5-JOptionPane/src/main/java/com/mycompany/pertemuan5/joptionpane/Pertemuan5JOptionPane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.pertemuan5.joptionpane;
+
+/**
+ *
+ * @author D-8
+ */
+public class Pertemuan5JOptionPane {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
